be7b8dd62e33dad46f45760db314bad51c53275f
CJPohl <cjpohldev@gmail.com>
2021-09-14 12:40:34 -0400

    Completed Library Project

---
+++ README.md
@@ -0,0 +1,14 @@
+# Library - Gutreads
+
+### Purpose
+This is my approach to the Library project assigned by The Odin Project.
+
+### Features
+- Input a book of choice with details like 'Title', 'Author', 'Pages', and 'Read/Not Read'
+- Keeps track of all your books
+- Change read status
+- Delete book from chart
+- Counter for total books!
+
+### Reflection
+This project helped me hone my abilites with working with arrays and object constructors.  While the localstorage API isn't used for this project, it still helped me get my feet wet with JS objects.
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,88 +1,167 @@
-let myLibrary = [];
+let myLibrary = []; // main object array
 
+// locate container variablers
 const container = document.querySelector('.container');
+const content = document.querySelector('.content');
 
-
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read) { // object constructor
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
-    }
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkein', '295', 'Read');
-const harryPotter = new Book('The Sorcerors Stone', 'J.K. Rowling', '234', 'Not Read');
-const theShining = new Book('The Shining', 'Steven King', '430', 'Not Read');
+function makeChart() { // function to show array object content
+    if (myLibrary.length == 0) { // if array is empty
+        const chart = document.createElement('div');
+        const info = document.createElement('div')
 
-myLibrary.push(theHobbit);
-myLibrary.push(harryPotter);
-myLibrary.push(theShining);
+        chart.classList.add('chart');
+        info.classList.add('empty');
 
-console.log(myLibrary);
+        info.textContent = 'No books yet :(';
 
-
-function makeChart() {
-    for (let i=0; i<myLibrary.length; i++) {
+        chart.appendChild(info);
+        container.appendChild(chart);
+    
+    }
+    
+    for (let i=0; i<myLibrary.length; i++) { // loop through the array and display all information for each book object and buttons
         const chart = document.createElement('div');
         const readBtn = document.createElement('button');
         const info = document.createElement('div');
         const del = document.createElement('button');
+
         chart.classList.add('chart');
-        chart.dataset.item = `book${i}`;
         readBtn.classList.add('readBtn');
         readBtn.dataset.item = `${i}`;
         info.classList.add('info');
         del.classList.add('del');
         del.dataset.item = `${i}`;
+
         readBtn.textContent = 'Read';
         info.textContent = `${myLibrary[i].title} | ${myLibrary[i].author} | ${myLibrary[i].pages} | ${myLibrary[i].read}`;
         del.textContent = 'Delete';
+
         chart.appendChild(readBtn);
         chart.appendChild(info);
         chart.appendChild(del);
         container.appendChild(chart);
 
+        // event listeners for buttons
         readBtn.addEventListener('click', changeReadStatus);
         del.addEventListener('click', deleteBook);
     }
+
+    if (document.querySelector('.add') == null) { // if no add button  add it
+        addAddBtn();
+    }
+
+    if (document.querySelector('.total') == null) {
+        updateTotalBooks();
+    }
 }
 
-function clearChart() {
+function addAddBtn() { // to replace add button
+    const addBtn = document.createElement('button')
+    addBtn.classList.add('add');
+    addBtn.textContent = 'Add Book';
+    content.appendChild(addBtn);
+    addBtn.addEventListener('click', addBookToLibrary);
+}
+
+function clearChart() { // clear current chart
     container.textContent = '';
 }
 
-function deleteBook() {
+function addBookToLibrary() { // function to create forms for user input for new book object
+    const formContainer = document.createElement('div');
+    formContainer.classList.add('formContainer');
+    
+    document.querySelector('.add').remove();
+    document.querySelector('.total').remove();
+
+    const addBook = document.createElement('form'); // form element
+
+    const titleInput = document.createElement('input');
+    titleInput.setAttribute('type', 'text');
+    titleInput.setAttribute('id', 'title');
+    titleInput.setAttribute('placeholder', 'Title');
+
+    const authorInput = document.createElement('input');
+    authorInput.setAttribute('type', 'text');
+    authorInput.setAttribute('id', 'author');
+    authorInput.setAttribute('placeholder', 'Author');
+
+    const pagesInput = document.createElement('input');
+    pagesInput.setAttribute('type', 'text');
+    pagesInput.setAttribute('id', 'pages');
+    pagesInput.setAttribute('placeholder', 'Pages');
+
+    const readInput = document.createElement('input');
+    readInput.setAttribute('type', 'text');
+    readInput.setAttribute('id', 'read');
+    readInput.setAttribute('placeholder', 'Read/Not Read');
+
+    const submitBtn = document.createElement('button')
+    submitBtn.classList.add('submitBtn');
+    submitBtn.textContent = 'Submit';
+    
+    addBook.appendChild(titleInput);
+    addBook.appendChild(authorInput);
+    addBook.appendChild(pagesInput);
+    addBook.appendChild(readInput);
+    formContainer.appendChild(addBook);
+    formContainer.appendChild(submitBtn);
+    content.appendChild(formContainer);
+    
+    submitBtn.addEventListener('click', newBook);
+}
+
+function newBook() { // when submit button is clicked, add new book object to array
+    
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const read = document.getElementById('read').value;
+
+    const book = new Book(`${title}`, `${author}`, `${pages}`, `${read}`);
+    myLibrary.push(book);
+    
+    document.querySelector('.formContainer').remove();
+    clearChart();
+    makeChart();
+}
+
+function deleteBook() { // remove selected book object from array
     const del = this.dataset.item;
-    for (let i=del; i<(del+1); i++) {
-        myLibrary.splice(`${i}`, 1);
-    }
+    myLibrary.splice(`${del}`, 1);
     clearChart();
     makeChart();
 }  
 
-function changeReadStatus() {
+function changeReadStatus() { // change selected book object read attribute
     const readBtn = this.dataset.item;
-    for (let i=readBtn; i<(readBtn+1); i++) {
-        if (myLibrary[i].read == 'Read') {
-            myLibrary[i].read = 'Not Read';
-        }
-        if (myLibrary[i].read == 'Not Read') {
-            myLibrary[i].read = 'Read';
-        }
-        
+    if (myLibrary[readBtn].read == 'Not Read') {
+        myLibrary[readBtn].read = 'Read';
     }
+    else {
+        myLibrary[readBtn].read = 'Not Read';
+    }
+
     clearChart();
     makeChart(); 
 }
 
+function updateTotalBooks() { // uses array size to update total book count
+    const bookTotal = document.createElement('div');
+    bookTotal.classList.add('total');
+    bookTotal.textContent = `${myLibrary.length} total books!`;
 
+    content.appendChild(bookTotal);
+}
 
-
-
-
-window.onload = function() {
+window.onload = function() { // make chart on page load
     makeChart();
+
 };
